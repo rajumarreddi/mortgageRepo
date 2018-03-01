ca6ad75f65a2776da61cdc8414c570fb3f05eb0f
Vinay K <vinaykallepalli@gmail.com>
2018-03-01 17:48:28 +0530

    dashboard and error messages

--- src/app/layout/form/form.component.ts
+++ src/app/layout/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MortgageInfo } from "./mortgageInfo";
 
 @Component({
     selector: 'app-form',
@@ -8,7 +10,24 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class FormComponent implements OnInit {
-    constructor() {}
+    mortgageInfo:FormGroup;
 
-    ngOnInit() {}
+    constructor(private fb:FormBuilder) {
+        
+    }
+
+    ngOnInit() {
+        this.createForm();
+    }
+
+    createForm(){
+        this.mortgageInfo=this.fb.group({
+        address:['', Validators.required]
+        });
+    }
+
+    onSubmit({ value, valid }: { value: MortgageInfo, valid: boolean }){
+        console.log(value.address);
+    }
+
 }
--- src/app/layout/form/form.module.ts
+++ src/app/layout/form/form.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { FormRoutingModule } from './form-routing.module';
 import { FormComponent } from './form.component';
 import { PageHeaderModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, FormRoutingModule, PageHeaderModule],
+    imports: [CommonModule, FormRoutingModule,
+        FormsModule, ReactiveFormsModule, PageHeaderModule],
     declarations: [FormComponent]
 })
 export class FormModule {}
